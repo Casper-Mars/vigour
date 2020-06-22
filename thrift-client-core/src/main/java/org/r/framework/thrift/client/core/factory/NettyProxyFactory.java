575ab9fca5b0c307d736eb6c12e2719ffc0c93d5
capser <solitary@163.com>
2020-06-22 18:26:46 +0800

    feat:添加nifty的客户端原理描述图

--- thrift-client-core/src/main/java/org/r/framework/thrift/client/core/factory/NettyProxyFactory.java
+++ thrift-client-core/src/main/java/org/r/framework/thrift/client/core/factory/NettyProxyFactory.java
@@ -1,11 +1,9 @@
 package org.r.framework.thrift.client.core.factory;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.*;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import org.r.framework.thrift.netty.ThriftChannelInitializer;
-import org.r.framework.thrift.netty.codec.ThriftProtocolDecoder;
-import org.r.framework.thrift.netty.codec.ThriftProtocolEncoder;
 
 /**
  *
@@ -17,13 +15,9 @@ public class NettyProxyFactory {
 
 
     public Bootstrap buildProxy(EventLoopGroup workThreads){
-
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.group(workThreads).channel(SocketChannel.class);
-
-
         ThriftChannelInitializer channelInitializer = new ThriftChannelInitializer();
-
         bootstrap.handler(channelInitializer);
         return bootstrap;
     }
