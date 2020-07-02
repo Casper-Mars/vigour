ce61a2e0d000cbbd3044b28525a98d37ac9e8309
reachlucifer <solitaryreach@163.com>
2020-07-02 15:38:01 +0800

    feat:删除原来的eureka模块

---
+++ test/test-client/src/main/java/test/client/TestServcie.java
@@ -0,0 +1,24 @@
+package test.client;
+
+import org.apache.thrift.TException;
+import org.r.framework.thrift.springboot.starter.annotation.ThriftService;
+import org.springframework.beans.factory.annotation.Value;
+import test.iface.Request;
+import test.iface.service.LoginService;
+
+/**
+ * date 20-4-30 下午4:15
+ *
+ * @author casper
+ **/
+@ThriftService
+public class TestServcie implements LoginService.Iface{
+
+    @Value("${server.port}")
+    private int port;
+
+    @Override
+    public String doAction(Request request) throws TException {
+        return String.valueOf(port);
+    }
+}
